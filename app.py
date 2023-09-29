bdf0fcd7c8a7d7fe3f3b732590d458820f99d9a8
Andrey <akuznecov9000@gmail.com>
2023-09-29 20:42:46 +0300

    Fin

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def login():
             records = list(cursor.fetchall())
             if not records:
                 return render_template('invalidlogin.html')
-            return render_template('account.html', full_name=records[0][1])
+            return render_template('account.html', full_name=records[0][1], login=records[0][2], password=records[0][3])
         elif request.form.get("registration"):
             return redirect("/registration")
     return render_template('login.html')
